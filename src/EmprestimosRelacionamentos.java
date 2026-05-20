b73b9fa15639ba9dc6ebe7c3d7bfd6f46c144920
Matheus-Bina <matheusbina8@gmail.com>
2026-05-20 00:25:45 -0300

    Mudança nome arquivo

--- src/EmprestimosRelacionamentos.java
+++ src/EmprestimosRelacionamentos.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Revisao {
+public class EmprestimosRelacionamentos {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
